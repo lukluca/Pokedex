3585aec67443d30a03e96d7ecaa475bd96aac9fc
Luca Tagliabue <homobonus-luca@hotmail.it>
2020-09-30 23:58:49 +0200

    Remove main storyboard and use a CollectionViewController as root view controller

--- Pokedex/AppDelegate.swift
+++ Pokedex/AppDelegate.swift
@@ -9,9 +9,13 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = PokemonCollectionViewController(collectionViewLayout: UICollectionViewFlowLayout())
+        window?.makeKeyAndVisible()
         return true
     }
 
--- Pokedex/PokemonCollectionViewController.swift
+++ Pokedex/PokemonCollectionViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PokemonCollectionViewController.swift
 //  Pokedex
 //
 //  Created by Luca Tagliabue on 30/09/2020.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PokemonCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
 
