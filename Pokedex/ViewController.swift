ed618dc532f6e502c56eb21d68088d9a59750ae0
Luca Tagliabue <homobonus.luca@gmail.com>
2020-09-30 21:34:53 +0200

    Xcode project init

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Luca Tagliabue on 30/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
