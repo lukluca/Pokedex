8229a7a907c033136e2267e6af574ee266ff9682
Luca Tagliabue <luca.tagliabue@bandyer.com>
2020-10-07 12:21:36 +0200

    Add DataDownloader protocol in order to centralize the download task

---
+++ Pokedex/Services/Downloader/DataDownloader.swift
@@ -0,0 +1,13 @@
+//
+//  DataDownloader.swift
+//  Pokedex
+//
+//  Created by Luca Tagliabue on 07/10/2020.
+//
+//
+
+import Foundation
+
+protocol DataDownloader {
+    func download(from url: URL, completion: @escaping (Data?) -> Void)
+}
