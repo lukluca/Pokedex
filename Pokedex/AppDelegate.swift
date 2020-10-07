47f8a1e32d4ecda40e93bd6a9ff20e2de36ea264
Luca Tagliabue <luca.tagliabue@bandyer.com>
2020-10-08 01:49:19 +0200

    Fix wrong init of two UIWindow

--- Pokedex/AppDelegate.swift
+++ Pokedex/AppDelegate.swift
@@ -13,6 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if #available(iOS 13.0, *) {
+            return true
+        }
+        
         window = MainWindowFactory().presentWithCollectionEmbeddedInNavigation()
         return true
     }
